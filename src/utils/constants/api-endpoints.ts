ca123250bd8755aed618e1e67945236c7b3e2297
neeraj-dev-007 <sangwan.neeraj2022@gmail.com>
2024-04-27 22:05:07 +0530

    feat: API endpoints

---
+++ src/utils/constants/api-endpoints.ts
@@ -0,0 +1,17 @@
+/* API Endpoints */
+
+//departments endpoints
+export const DEPARTMENTS = 'departments';
+export const DEPARTMENT_ID = ':departmentId';
+
+//employees endpoints
+export const EMPLOYEES = 'employees';
+export const EMPLOYEE_ID = ':employeeId';
+
+//shifts endpoints
+export const SHIFTS = 'shifts';
+export const SHIFT_ID = ':shiftId';
+
+//roles endpoints
+export const ROLES = 'roles';
+export const ROLE_ID = ':roleId';
