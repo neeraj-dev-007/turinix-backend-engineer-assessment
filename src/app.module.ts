66d4f9915a1b25d3644e0939f15eed5d0a5ff475
neeraj-dev-007 <sangwan.neeraj2022@gmail.com>
2024-04-27 21:23:10 +0530

    feat: logging
    Added logging feature.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RolesModule } from './components/roles/roles.module';
+import { DepartmentsModule } from './components/departments/departments.module';
+import { EmployeesModule } from './components/employees/employees.module';
+import { ShiftsModule } from './components/shifts/shifts.module';
+import { LoggerMiddleware } from './utils/middlewares/logger/logger.middleware';
+import { DEPARTMENTS } from './utils/constants/api-endpoints';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { Department } from './components/departments/department.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    RolesModule,
+    DepartmentsModule,
+    EmployeesModule,
+    ShiftsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: 'postgres://jojzmzjv:dpslUnGbuNoVX-emvHKKDtIJO5zR7Csr@kiouni.db.elephantsql.com/jojzmzjv',
+      username: 'jojzmzjv',
+      password: 'dpslUnGbuNoVX-emvHKKDtIJO5zR7Csr',
+      database: 'jojzmzjv',
+      synchronize: true,
+      entities: [Department],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  constructor(private dataSource: DataSource) {
+    console.log(dataSource.driver.database);
+  }
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(DEPARTMENTS);
+  }
+}
